e8bc503b9be81254b51d8352b2ace55ad42d7ae0
Chelsea Merrill <72108067+ChelseaMerrill@users.noreply.github.com>
2022-10-13 16:10:25 -0400

    skeleton

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,42 @@
+
 <script setup lang="ts">
 
 </script>
 
 <template>
   <CContainer>
-    <CRow>
-      <CCol sm="auto">
-        <h1>Compare Two versions of the same Bill</h1>
+    <CRow class="bill-form-row">
+      <CCol>
+        <h3>Compare Two versions of the same Bill</h3>
         <div class="bill-form-col">
-
+          <div class="version-field">
+            <p>First Version</p>
+            <input/>
+          </div>
+          <div class="version-field">
+            <p>Second Version</p>
+            <input/>
+          </div>
+          <button class="compare-button">Compare</button>
         </div>
       </CCol>
 
-      <CCol sm="auto">
-        <h1>Compare different Bills and Sessions</h1>
+      <CCol>
+        <h3>Compare different Bills and Sessions</h3>
         <div class="bill-form-col">
-
+          <div class="bill-group">
+            <p>First Bill</p>
+            <div class="bill-input-row"><p>Bill:</p><input/></div>
+            <div class="bill-input-row"><p>Version:</p><input/></div>
+            <div class="bill-input-row"><p>Session:</p><input/></div>
+          </div>
+          <div class="bill-group">
+            <p>Second Bill</p>
+            <div class="bill-input-row"><p>Bill:</p><input/></div>
+            <div class="bill-input-row"><p>Version:</p><input/></div>
+            <div class="bill-input-row"><p>Session:</p><input/></div>
+          </div>
+          <button class="compare-button">Compare</button>
         </div>
       </CCol>
     </CRow>
@@ -23,11 +44,32 @@
 </template>
 
 
-
+<style src="bootstrap/dist/css/bootstrap.min.css"></style>
 <style>
+.bill-form-row {
+  display: flex;
+  justify-content: space-between;
+  text-align: center;
+  font-family: Arial, Helvetica, sans-serif;
+}
 .bill-form-col {
-  border: 2px solid rgb(10, 10, 10);
-  height: 70%;
-  width: 40%;
+  border: 2px solid rgb(48, 48, 48);
+  height: 80vh;
+  width: 600px;
+  border-radius: 5px;
+  background-color: rgb(180, 216, 251);
+}
+.compare-button{
+  background-color: black;
+  color: white;
+  width: 200px;
+  border: 2px white;
+  border-radius: 5px;
+}
+.bill-input-row{
+  display: flex;
+  justify-content: space-between;
+  padding-left: 150px;
+  padding-right: 150px;
 }
 </style>
