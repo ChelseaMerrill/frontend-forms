61b59b4caac3ed695f1548d26ada42756ba37a9f
Chelsea Merrill <72108067+ChelseaMerrill@users.noreply.github.com>
2022-10-13 14:43:02 -0400

    install core ui and begin grid

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,33 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+
 </script>
 
 <template>
-<h1>Hello world</h1>
+  <CContainer>
+    <CRow>
+      <CCol sm="auto">
+        <h1>Compare Two versions of the same Bill</h1>
+        <div class="bill-form-col">
+
+        </div>
+      </CCol>
+
+      <CCol sm="auto">
+        <h1>Compare different Bills and Sessions</h1>
+        <div class="bill-form-col">
+
+        </div>
+      </CCol>
+    </CRow>
+  </CContainer>
 </template>
 
+
+
+<style>
+.bill-form-col {
+  border: 2px solid rgb(10, 10, 10);
+  height: 70%;
+  width: 40%;
+}
+</style>
