a47bd6d00843699630cfaf0f13e8c152147f4556
Chelsea Merrill <72108067+ChelseaMerrill@users.noreply.github.com>
2022-10-13 13:55:48 -0400

    first commit

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+<h1>Hello world</h1>
+</template>
+
