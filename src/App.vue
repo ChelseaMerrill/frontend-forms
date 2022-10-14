879e6a4affcb43aeabad496db36a70c240c6b196
Chelsea Merrill <72108067+ChelseaMerrill@users.noreply.github.com>
2022-10-14 15:59:13 -0400

    moved data set

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,35 @@
 
 <script setup lang="ts">
+const ArrayOne = [
+    {
+        pageLine: 'Page 1 Line 1',
+        content: 'removedContent'
+    },
+    {
+        pageLine: "Page 1 Line 3",
+        content: 'this content was also removed'
+    },
+    {
+        pageLine: "Page 2 Line 3",
+        content: 'this content was also removed'
+    }
+]
+
+const ArrayTwo = [
+    {
+        pageLine: 'Page 1 Line 5',
+        content: 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using'
+    },
+    {
+        pageLine: "Page 5 Line 3",
+        content: 'this content was added'
+    },
+    {
+        pageLine: "Page 5 Line 4",
+        content: 'It is a web page editors now use Lorem Ipsum as their default model text, and a search will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).'
+    }
+
+]
 
 </script>
 
