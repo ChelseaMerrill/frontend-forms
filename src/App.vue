b356b3b5ce606da54389ae3ebe49c516cb9dd829
Chelsea Merrill <72108067+ChelseaMerrill@users.noreply.github.com>
2022-10-17 10:04:20 -0400

    fixed page-line

--- src/App.vue
+++ src/App.vue
@@ -76,7 +76,7 @@ const ArrayTwo = [
   <CContainer class="diff-container">
     <div class="diff-col diff-col-one">
       <div class="page-line-heading">
-        <span>Page</span>
+        <span>Page 1 Line 15</span>
       </div>
       <div class="changed-text removed-text">
         <p>changedtext</p>
@@ -84,7 +84,7 @@ const ArrayTwo = [
     </div>
     <div class="diff-col diff-col-two">
       <div class="page-line-heading">
-        <span>Page</span>
+        <span>Page 2 Line 1</span>
       </div>
       <div class="changed-text added-text">
         <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</p>
